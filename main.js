7ae545f12489ecb9bc61cb61061a6f712347792b
Samuel Trevan <samuel.trevan@gmail.com>
2022-10-07 10:15:50 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const dishes = [
+    "dinner plate", "Water glass", "Salad bowl", "dinner plate", "Wine glass", "Spoon", "Spoon", "Fork", "Salad bowl", "Whiskey glass", "Fork", "Spoon", "Fork"
+]
+
+const waterGlass = dishes[1]
+const wineGlass = dishes[4]
+const whiskeyGlass = dishes[9]
+
+console.log("I am cleaning the following glasses: ")
+console.log(waterGlass)
+console.log(wineGlass)
+console.log(whiskeyGlass)
